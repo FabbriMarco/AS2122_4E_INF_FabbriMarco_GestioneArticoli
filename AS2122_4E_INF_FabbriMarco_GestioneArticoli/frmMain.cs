19db80b9e1d8a195abfc5a6e66cf751417be05f0
PC06 <PC06@INFO5-5392>
2022-05-18 09:15:43 +0200

    Aggiungere i file di progetto.

---
+++ AS2122_4E_INF_FabbriMarco_GestioneArticoli/frmMain.cs
@@ -0,0 +1,10 @@
+namespace AS2122_4E_INF_FabbriMarco_GestioneArticoli
+{
+    public partial class frmMain : Form
+    {
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+    }
+}
